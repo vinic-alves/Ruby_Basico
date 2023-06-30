11ad7deafc8f36467485e2f04955ece89bbc4bfe
vinic-alves <vinicius.alves95@outlook.com>
2023-06-29 22:24:51 -0300

    ruby básico 3

---
+++ hashes.rb
@@ -0,0 +1,11 @@
+hash = {"keys", "valor"}
+
+#forma de fazer o hash 
+hashes = {"a" => "1", "b"=>2}
+#imprimindo o valor
+hashes["a"]
+#passando como simbolos:
+hashes = {a: => "1", :b=>2}
+hashes[a:]
+
+
---
+++ vetores_e_arrays.rb
@@ -0,0 +1,16 @@
+vetor = [1, 2, 3,4,5]
+
+vetor.each do |i|
+    puts i 
+end 
+
+(1...5).each do |i|
+    puts i
+
+end
+
+(1..5).each do |i|
+    puts i  
+end
+
+5.times{puts 'bruno'}
